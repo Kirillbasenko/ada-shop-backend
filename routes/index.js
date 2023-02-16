6061316a8069380068c57aad7a35f0a79d256524
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-02-16 13:37:53 +0200

    first commit1

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const Router = require("express")
+const router = new Router()
+const brandRouter = require("./brandRouter")
+const deviceRouter = require("./deviceRouter")
+const typeRouter = require("./typeRouter")
+const userRouter = require("./userRouter")
+const reviewRouter = require("./reviewRouter")
+
+router.use("/user", userRouter)
+router.use("/type", typeRouter)
+router.use("/brand", brandRouter)
+router.use("/device", deviceRouter)
+router.use("/review", reviewRouter)
+
+module.exports = router
