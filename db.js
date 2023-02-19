240705273e750e16a4a4ccf1bc2c1b9b2c7f5388
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-02-19 11:54:22 +0200

    vercel

--- db.js
+++ db.js
@@ -11,5 +11,5 @@ const {Sequelize} = require("sequelize")
    }
 )*/
 
-//module.exports = new Sequelize("postgres://postgres:elizaveta@localhost:5432/online-store")
-module.exports = new Sequelize("postgres://bzmxxzeebpsrmo:3df6032fbf5629f2fc383d2c8a44b6f776a84c2ced994c1302503db0ce666721@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d9mscsh1v2fom2")
+module.exports = new Sequelize("postgres://postgres:elizaveta@localhost:5432/online-store")
+//module.exports = new Sequelize("postgres://bzmxxzeebpsrmo:3df6032fbf5629f2fc383d2c8a44b6f776a84c2ced994c1302503db0ce666721@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d9mscsh1v2fom2")
