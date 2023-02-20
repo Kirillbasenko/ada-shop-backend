28266c5d293df5d40ac4ab491d28b312847f5bcb
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-02-20 17:29:05 +0200

    Elephant2

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const path = require("path")
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
+app.use(cors(({
+   origin: ["https://ada-shop-frontend.vercel.app"]
+})))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, "static")))
 app.use(fileUpload({}))
