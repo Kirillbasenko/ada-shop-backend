e9ea5168950bbce07bdd6c71f5ed981f9b7340b8
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-02-21 10:41:13 +0200

    Elephant14

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize")
 
-/*module.exports = new Sequelize(
+module.exports = new Sequelize(
    process.env.DB_NAME1,
    process.env.DB_USER1,
    process.env.DB_PASSWORD1,
@@ -9,7 +9,7 @@ const {Sequelize} = require("sequelize")
       host: process.env.DB_HOST1,
       port: process.env.DB_PORT1
    }
-)*/
+)
 
 //module.exports = new Sequelize("postgres://postgres:elizaveta@localhost:5432/online-store")
-module.exports = new Sequelize("postgres://tkmjtqtm:egdFSuE-xdapQfZSW2F-7NxfloAE7NuR@mouse.db.elephantsql.com/tkmjtqtm")
+//module.exports = new Sequelize("postgres://tkmjtqtm:egdFSuE-xdapQfZSW2F-7NxfloAE7NuR@mouse.db.elephantsql.com/tkmjtqtm")
